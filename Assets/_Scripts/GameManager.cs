5e0e923436f88afc76d3cd2b11242f07ba11ed5b
yamasho69 <nitiham55@yahoo.co.jp>
2022-11-03 02:12:14 +0900

    6回目のコミット
    クリア後の要素実装。文字のアニメーションを実装。

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -11,27 +11,30 @@ public class GameManager : MonoBehaviour {
 
 	// データセーブ用キー
 	private const string KEY_SCORE = "SCORE";	// スコア
-	private const string KEY_LEVEL = "LEVEL";	// レベル
-	private const string KEY_TIME = "TIME";		// 時間
+	private const string KEY_LEVEL = "LEVEL";   // レベル
+
+	//bool値をセーブデータとして扱う　https://smartgames.hatenablog.com/entry/2016/08/22/010140
+	private const string KEY_CLEAR = "CLEAR";   //クリア済か
 
 	// オブジェクト参照
 	public GameObject heartPrefab;		// ハートプレハブ
-	public GameObject smokePrefab;		// 煙プレハブ
-	public GameObject kusudamaPrefab;	// くす玉プレハブ
-	public GameObject canvasGame;		// ゲームキャンバス
+	public GameObject lastText;	// ラストテキスト
 	public GameObject textScore;		// スコアテキスト
 	public GameObject [] imagePlace;		// ゆっくりプレイス
 	public GameObject [] tapYukkuri;        // タップするゆっくり
 	public int yukkuriNumber = 0; //ゆっくりの種類
-	public AudioClip levelUpSE;			// 効果音：レベルアップ
-	public AudioClip clearSE;			// 効果音：クリア
+	public AudioClip [] levelUpSE;			// 効果音：レベルアップ
+	public AudioClip clearSE;           // 効果音：クリア
+	public GameObject[] levelUpText;//レベルアップ時のテキスト
+	public GameObject particleBox; //パーティクルのフォルダ。レベルアップ時に非表示にする。
 
 	// メンバ変数
 	private int score = 0;			// 現在のスコア
 	private int nextScore = 10;		// レベルアップまでに必要なスコア
-	public int placeLevel = 0;	// プレイスのレベル
+	public int placeLevel = 0;  // プレイスのレベル
+	public bool isClear;
 
-	private int[] nextScoreTable = new int[] {20,30,40,50,99,999};
+	private int[] nextScoreTable = new int[] {20,30,40,50,99,100};
 									// レベルアップ値
 	private AudioSource audioSource;// オーディオソース
 
@@ -47,8 +50,16 @@ public class GameManager : MonoBehaviour {
 		// 初期設定
 		score = PlayerPrefs.GetInt (KEY_SCORE, 0);
 		placeLevel = PlayerPrefs.GetInt (KEY_LEVEL, 0);
+		int clearChack = PlayerPrefs.GetInt(KEY_CLEAR);
+		if(clearChack == 1) {
+			isClear = true;
+        }
+		if(isClear) {//クリア済の場合
+			ClearEffect();
+		} else {
+			nextScore = nextScoreTable[placeLevel];
+		}
 		placeText.text = "ゆっくりプレイス：" + placeString[placeLevel];
-		nextScore = nextScoreTable [placeLevel];
 		imagePlace[placeLevel].SetActive(true);
 		cYB.BackYukkuri();//後ろのゆっくりを表示
 		RefreshScoreText ();
@@ -62,6 +73,15 @@ public class GameManager : MonoBehaviour {
 	// ハート入手
 	public void GetHeart (int getScore) {
 
+		//クリア済の場合はこの処理だけしてリターン
+        if (isClear) {
+			score += getScore;
+			RefreshScoreText();
+			SaveGameData();
+			return;
+		}
+
+
 		if (score < nextScore) {
 			score += getScore;
 
@@ -69,31 +89,37 @@ public class GameManager : MonoBehaviour {
 			if (score > nextScore) {
 				score = nextScore;
 			}
-
-			placeLevelUp ();
-			RefreshScoreText ();
+			placeLevelUp();
+			RefreshScoreText();
 
 			// ゲームクリア判定
-			if ((score == nextScore) && (placeLevel == MAX_LEVEL)) {
+			if ((score == nextScore) && (placeLevel == MAX_LEVEL)&&!isClear) {//クリアしてないも条件に追加
 				ClearEffect ();
-			}
+            }
 		}
 		SaveGameData ();
 	}
 
 	// スコアテキスト更新
 	void RefreshScoreText () {
-		textScore.GetComponent<Text> ().text = 
-			"ゆっくり度：" + score + " / " + nextScore;
+		if (!isClear) {
+			textScore.GetComponent<Text>().text =
+				"ゆっくり度：" + score + " / " + nextScore;
+        } else {
+			textScore.GetComponent<Text>().text =
+				"ゆっくり度：" + score + " / ∞";
+		}
 	}
 
 	// プレイスのレベル管理
 	void placeLevelUp () {
 		if (score >= nextScore) {
 			if (placeLevel < MAX_LEVEL) {
+				particleBox.SetActive(false);
 				fadeController.isFadeOut = true;
-				audioSource.PlayOneShot(levelUpSE);
-				Invoke("FadeIn", 1.5f);
+				audioSource.PlayOneShot(levelUpSE[placeLevel]);
+				levelUpText[placeLevel].SetActive(true);
+				Invoke("FadeIn", 3.8f);
 			}
 		}
 	}
@@ -104,31 +130,35 @@ public class GameManager : MonoBehaviour {
 		imagePlace[placeLevel].SetActive(true);//次のプレイスを出す
 		score = 0;
 		nextScore = nextScoreTable[placeLevel];
+		RefreshScoreText();
 		placeText.text = "ゆっくりプレイス：" + placeString[placeLevel];
 		cYB.BackYukkuri();
+		levelUpText[placeLevel - 1].SetActive(false);
 		fadeController.isFadeIn = true;
+		particleBox.SetActive(true);
     }
-
-	// レベルアップ時の演出
-	void placeLevelUpEffect () {
-		GameObject smoke = (GameObject)Instantiate (smokePrefab);
-		smoke.transform.SetParent (canvasGame.transform, false);
-		smoke.transform.SetSiblingIndex (2);
-		audioSource.PlayOneShot (levelUpSE);
-		Destroy (smoke, 0.5f);		
-	}
 
 	// プレイスが最後まで育った時の演出
 	void ClearEffect () {
-		GameObject kusudama = (GameObject)Instantiate(kusudamaPrefab);
-		kusudama.transform.SetParent (canvasGame.transform, false);
+		//初クリア時のみスコアをリセットする。
+        if (!isClear) {
+			score = 0;
+        }
+		lastText.SetActive(true);
 		audioSource.PlayOneShot (clearSE);
+		nextScore = nextScoreTable[placeLevel];
+		RefreshScoreText();
+		isClear = true;
 	}
 
 	// ゲームデータをセーブ
 	void SaveGameData () {
 		PlayerPrefs.SetInt (KEY_SCORE, score);
 		PlayerPrefs.SetInt (KEY_LEVEL, placeLevel);
+        if (!isClear) {//クリア済なら1、未クリアなら0をキーに保存
+			PlayerPrefs.SetInt(KEY_CLEAR, 0);
+        } else { PlayerPrefs.SetInt(KEY_CLEAR, 1); }
+
 		PlayerPrefs.Save ();
 	}
 
